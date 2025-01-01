cc4af8daa3ed12ddf0a74f91c1159fb862250580
zafar-m3n <thezafar.m3n@gmail.com>
2025-01-01 19:41:13 +0530

    Footer complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,51 @@
 import React from "react";
-import logo from "@/assets/zipphy.png";
 import Icon from "@/components/ui/Icon";
+import logo from "@/assets/logo-white.png";
 
 const Footer = () => {
   return (
     <footer className="bg-primary text-background pt-8">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 pb-8 border-b border-secondary">
         <div className="flex flex-col items-start">
-          <img src={logo} alt="Zipphy Logo" className="w-32 mb-4" />
-          <div className="flex space-x-4">
+          <img src={logo} alt="Zipphy Logo" className="w-full" />
+          <div className="flex space-x-4 ms-12">
             <Icon icon="mdi:facebook" width={36} className="text-accent hover:scale-110 transition-transform" />
             <Icon icon="mdi:instagram" width={36} className="text-accent hover:scale-110 transition-transform" />
             <Icon icon="mdi:linkedin" width={36} className="text-accent hover:scale-110 transition-transform" />
           </div>
         </div>
         <div>
-          <h3 className="text-lg font-semibold text-accent mb-4">Contact Us</h3>
-          <ul className="space-y-2">
-            <li>Email: contact@zipphy.com</li>
-            <li>Phone: +123 456 7890</li>
-            <li>Address: 123 Zipphy Lane, Green City</li>
-          </ul>
+          <h3 className="text-lg font-bold text-accent mb-6 underline underline-offset-8">Contact Us</h3>
+          <div className="flex items-center mb-6">
+            <Icon icon="heroicons:envelope" className="bg-accent w-11 h-11 p-2 rounded-full" />
+            <span className="ml-4 text-background">support@zipphy.com</span>
+          </div>
+          <div className="mb-6">
+            <h4 className="text-base font-semibold text-background mb-2">Australia</h4>
+            <div className="flex items-start mb-2">
+              <Icon icon="heroicons:map-pin" className="bg-accent w-12 h-11 p-2 rounded-full" />
+              <p className="ml-4 text-background">57 Carters Road, Dural NSW 2158, Sydney, Australia.</p>
+            </div>
+            <div className="flex items-center">
+              <Icon icon="heroicons:phone" className="bg-accent w-11 h-11 p-2 rounded-full" />
+              <span className="ml-4 text-background">+61 483 956 413</span>
+            </div>
+          </div>
+          <div className="mb-6">
+            <h4 className="text-base font-semibold text-background mb-2">United Kingdom</h4>
+            <div className="flex items-start mb-2">
+              <Icon icon="heroicons:map-pin" className="bg-accent w-11 h-11 p-2 rounded-full" />
+              <p className="ml-4 text-background">22 Broomfield Place, Stoven, United Kingdom.</p>
+            </div>
+            <div className="flex items-center">
+              <Icon icon="heroicons:phone" className="bg-accent w-11 h-11 p-2 rounded-full" />
+              <span className="ml-4 text-background">+44 7360 545857</span>
+            </div>
+          </div>
         </div>
 
-        {/* Third Column */}
         <div>
-          <h3 className="text-lg font-semibold text-accent mb-4">Useful Links</h3>
+          <h3 className="text-lg font-bold text-accent mb-6 underline underline-offset-8">Useful Links</h3>
           <ul className="space-y-2">
             <li>
               <a href="/" className="hover:text-accent">
